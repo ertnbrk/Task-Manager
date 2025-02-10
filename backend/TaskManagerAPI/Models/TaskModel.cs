300683ff2f25397a990cf4cd33cba51a3818d1cd
Ertan Burak FELEK <98287486+ertnbrk@users.noreply.github.com>
2025-02-11 01:27:25 +0300

    Removed old backend files

--- backend/TaskManagerAPI/Models/TaskModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class TaskModel
-{
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public bool IsCompleted { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
